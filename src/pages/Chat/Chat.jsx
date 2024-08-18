156de6fd300113e2ad968c3a44b5718c536f6ee5
Abhishek <abhishek.das0023@gmail.com>
2024-08-19 00:40:13 +0600

    New update

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import './Chat.css'
-// import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
-// import ChatBox from '../../components/ChatBox/ChatBox'
-// import RightSidebar from '../../components/RightSidebar/RightSidebar'
+import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
+import ChatBox from '../../components/ChatBox/ChatBox'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
 
 const Chat = () => {
   return (
     <div className='chat'>
         <div className="chat-container">
           <LeftSidebar/>
-          {/* <ChatBox/> */}
-          {/* <RightSidebar/> */}
+          <ChatBox/>
+          <RightSidebar/>
         </div>
     </div>
   )
