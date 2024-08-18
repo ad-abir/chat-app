32097449918a5fcfec62a68237230c9e169b6fa6
Abhishek <abhishek.das0023@gmail.com>
2024-08-18 23:46:30 +0600

    New update

---
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './LeftSidebar.css'
+import assets from '../../assets/assets'
+
+const LeftSidebar = () => {
+  return (
+    <div className='ls'>
+        <div className="ls-top">
+            <div className="ls-nav">
+                <img src={assets.logo} className='logo' alt="" />
+                <div className="menu">
+                    <img src={assets.menu_icon} alt="" />
+                </div>
+            </div>
+            <div className="ls-search">
+                <img src={assets.search_icon} alt="" />
+                <input type="text" placeholder="Search" />
+            </div>
+        </div>
+        <div className="ls-list">
+            <div className="friends">
+                <img src={assets.profile_img} alt="" />
+                <div>
+                    <p>Richard Sanford</p>
+                    <span>Hello, how are you?</span>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default LeftSidebar
