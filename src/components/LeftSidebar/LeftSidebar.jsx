d560a61286dac9eb96973f03360a00dcd430ab30
Abhishek <abhishek.das0023@gmail.com>
2024-08-19 00:39:54 +0600

    New update

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -18,13 +18,15 @@ const LeftSidebar = () => {
             </div>
         </div>
         <div className="ls-list">
-            <div className="friends">
+            {Array(12).fill("").map((item,index)=>(
+                <div key={index} className="friends">
                 <img src={assets.profile_img} alt="" />
                 <div>
                     <p>Richard Sanford</p>
                     <span>Hello, how are you?</span>
                 </div>
             </div>
+            ))}
         </div>
     </div>
   )
