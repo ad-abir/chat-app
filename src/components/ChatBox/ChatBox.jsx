2c553b7c78c1e34b48b0263130cd8d6529aae352
Abhishek <abhishek.das0023@gmail.com>
2024-08-21 01:19:05 +0600

    Progress in Chat page

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -19,9 +19,15 @@ const ChatBox = () => {
             <p>2:30 PM</p>
           </div>
         </div>
-      </div>
 
-      <div className="chat-msg">
+        <div className="s-msg">
+          <img className="msg-img" src={assets.pic1} alt="" />
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+
         <div className="r-msg">
           <p className="msg">Hello, how are you?...</p>
           <div>
