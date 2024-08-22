17af2633b5ed569f373e4d462a125212a340cad7
Abhishek <abhishek.das0023@gmail.com>
2024-08-23 01:30:02 +0600

    Progress in Profile page

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
 
 const ProfileUpdate = () => {
+
+  const [image, setImage] = useState(false);
+
   return (
-    <div>
-        We are on the Profile Page
+    <div className='profile'>
+        <div className="profile-container">
+          <form>
+            <h3>Profile Details</h3>
+            <label htmlFor="avatar">
+              <input onChange={(e) => setImage(e.target.files[0])} type="file" id='avatar' accept='.png,.jpg,.jpeg' hidden/>
+              <img src={image? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
+              upload profile image
+            </label>
+            <input type="text" placeholder='Your name' required/>
+            <textarea placeholder='Write your bio' required></textarea>
+            <button type="submit">Update</button>
+          </form>
+          <img className="profile-pic" src={image? URL.createObjectURL(image) : assets.logo_icon} alt="" />
+        </div>
     </div>
   )
 }
